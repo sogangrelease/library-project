434f7f426ce119f4abadea8092190f0b70bc4391
DHL0103 <daehee2005@gmail.com>
2025-11-04 11:00:27 +0900

    login complete

--- Library/src/main/java/com/release/library/member/MemberController.java
+++ Library/src/main/java/com/release/library/member/MemberController.java
@@ -1,32 +1,65 @@
 package com.release.library.member;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.security.Principal;
+
+
 @RequiredArgsConstructor
 @Controller
-@RequestMapping("/user")
+@RequestMapping("/member")
 public class MemberController {
     private final MemberService memberService;
 
+    //로그인
+    @GetMapping("/login")
+    public String login(){
+        return "login_page";
+    }
+
     //계정 생성 (관리자만 가능)
     @PostMapping("/create")
-    public String create() {
-
+    public String create(@RequestParam("studentId") String studentId,
+                         @RequestParam("password") String password) {
+        this.memberService.create(studentId, password);
         //관리자페이지
-        return "redirect:/admin/page";
+        return "redirect:/member/login";
     }
 
     //비밀번호 변경
     @PostMapping("/changepw")
-    public String changePW(@RequestParam("oldpw") String oldpw, @RequestParam("newpw") String newpw) {
+    public String changePW(@RequestParam("oldpw") String oldpw,
+                           @RequestParam("newpw") String newpw,
+                           Principal principal,
+                           HttpServletRequest request) {
+
+        //현재 로그인 되어있는 유저 member로 가져오기
+        Member member = this.memberService.getMember(principal.getName());
+        //암호화 되어있는 비밀번호 조회 및 생성 위해 가져오기
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
 
+        //비번 불일치 하면 마이페이지로 돌아가기
+        if(!passwordEncoder.matches(oldpw,member.getPasswordHash())){
+            return "redirect:/member/mypage";
+        }
+
+        //이제 이전 비밀번호가 일치하는 것을 알기 때문에 해당 멤버 객체와 새로운 비밀번호를 서비스로 넘겨준다.
+        this.memberService.changePassword(member,newpw);
+
+        //세션 종료시키기 (로그 아웃 시키기)
+        request.getSession().invalidate();
+        SecurityContextHolder.clearContext();
         //다시 로그인 화면으로 이동
-        return "login_page";
+        return "redirect:/member/login";
     }
 
 
--- Library/src/main/java/com/release/library/member/MemberService.java
+++ Library/src/main/java/com/release/library/member/MemberService.java
@@ -2,6 +2,7 @@ package com.release.library.member;
 
 import com.release.library.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,14 +12,35 @@ import java.util.Optional;
 public class MemberService {
     private final MemberRepository memberRepository;
 
-    //유저 찾아서 반환
+    //유저 찾아서 반환 (학번으로 찾음)
     public Member getMember(String studentId) {
-            Optional<Member> member = this.memberRepository.findByStudentId(studentId);
+        Optional<Member> member = this.memberRepository.findByStudentId(studentId);
         if ( member.isPresent() ) {
             return member.get();
         }
         else {
-            throw new DataNotFoundException( "Member Not Found~ !!" );
+            throw new DataNotFoundException( "Member Not Found!!" );
         }
+    }
+
+
+    // 비밀번호 보안 처리 후 데이터베이스에 저장
+    public void create(String studentId, String password) {
+        //비밀번호 암호화 후 이름과 비번 저장
+        Member member = new Member();
+        member.setStudentId(studentId);
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        member.setPasswordHash(passwordEncoder.encode(password));
+        //데이터베이스에 저장
+        this.memberRepository.save(member);
+    }
+
+    //비밀번호 변경
+    public void changePassword(Member member ,String newpw){
+        //비밀번호 암호화
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        member.setPasswordHash(passwordEncoder.encode(newpw));
+        //데이터베이스에 저장
+        this.memberRepository.save(member);
     }
 }
