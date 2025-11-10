a7ee9d2324ddd7c13346007b032701f3744c1d74
skyflowlab <179109559+skyflowlab@users.noreply.github.com>
2025-11-10 21:25:04 +0900

    MyPage 2

--- src/MyPage.jsx
+++ src/MyPage.jsx
@@ -19,24 +19,27 @@ function MyPage() {
 				<header>
 					<p>대출 현황</p>
 				</header>
-				<ul>{books}</ul>
+				<div className="content">
+					<ul>{books}</ul>
+				</div>
 			</div>
 			<div className="box">
 				<header>
 					<p>정보 수정</p>
 				</header>
-				<form>
-					<div className='input'>
-						<input type='text' disabled placeholder={account.이름} name='이름' prefix='이름' />
-					</div>
-					<input type='text' disabled placeholder={account.학번} name='학번' prefix='이름' />
-					<input type='text' disabled placeholder={account.전화번호} name='전화번호' prefix='이름' />
-				</form>
-				<form>
-					<input type='password' name='현재 비밀번호' required />
-					<input type='password' name='변경할 비밀번호' required />
-					<input type='password' name='변경할 비밀번호 확인' required />
-				</form>
+				<div className="content">
+					<form>
+						<input type='text' disabled placeholder='이름' value={account.이름} name='이름' prefix='이름' />
+						<input type='text' disabled placeholder='학번' value={account.학번} name='학번' prefix='이름' />
+						<input type='text' disabled placeholder='전화번호' value={account.전화번호} name='전화번호' prefix='이름' />
+					</form>
+					<form>
+						<input type='password' placeholder='현재 비밀번호' name='현재 비밀번호' required />
+						<input type='password' placeholder='변경할 비밀번호' name='변경할 비밀번호' required />
+						<input type='password' placeholder='변경할 비밀번호 확인' name='변경할 비밀번호 확인' required />
+					</form>
+					<button type='button' name='비밀번호 변경'>비밀번호 변경</button>
+				</div>
 			</div>
 		</div>
 	</>
