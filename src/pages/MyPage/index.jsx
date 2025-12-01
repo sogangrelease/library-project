984e3e6fb6b77ede0ee93fea4dffc74ef8a621d4
jiy2745 <jiy2745@gmail.com>
2025-12-01 19:11:00 +0900

    fix: add handleSubmitPassword function

--- src/pages/MyPage/index.jsx
+++ src/pages/MyPage/index.jsx
@@ -1,30 +1,72 @@
 import styles from './MyPage.module.css'; 
-import { useEffect } from 'react'; 
+import { useState, useEffect } from 'react'; 
 import api from '@/api/axios';
 import { Link } from 'react-router-dom';
 import releaseLogo from '@/assets/release-black-small.webp';
 
 function MyPage() {
-    let accountOnServer = {이름: '홍길동', 학번: '20231234', 전화번호: '010-1234-5678'};
-    let booksOnServer = [
+    const [isLoading, setIsLoading] = useState(true);
+    const [account, setAccount] = useState({name: '홍길동', studentId: '20231234', phoneNumber: '010-1234-5678'}); 
+    const [booksData, setBooksData] = useState([
         {borrowId: 1, titleMain: 'JavaScript 기초', returnAt: '2024-07-15'},
         {borrowId: 2, titleMain: 'React 입문', returnAt: '2024-07-20'},
         {borrowId: 3, titleMain: '웹 개발 완전 정복', returnAt: '2024-07-25'}
-    ];
+    ]);
+    const [passwordForm, setPasswordForm] = useState({
+        currentPassword: '',
+        newPassword: '',
+        confirmPassword: '',
+    });
 
-    useEffect(() => {
-        api.post('/my/borrow/list')
+    const handlePasswordChange = (e) => {
+        setPasswordForm({
+            ...passwordForm,
+            [e.target.name]: e.target.value
+        });
+
+        console.log(passwordForm);
+    };
+
+    const handleSubmitPassword = () => {
+        if (passwordForm.newPassword !== passwordForm.confirmPassword) {
+            alert("새 비밀번호가 일치하지 않습니다!");
+            return;
+        }
+
+        api.post('/member/changepw', {
+            'oldpw': passwordForm.currentPassword,
+            'newpw': passwordForm.newPassword
+        })
         .then(function (response) {
-            booksOnServer = response.data;
+            alert(response.data);
         })
         .catch(function (error) {
             console.error(error);
+            alert(`error: ${error}`);
         });
-    }, []);
-    
+    };
 
-    const account = accountOnServer;
-    const books = booksOnServer.map(book =>
+    useEffect(() => {
+        api.post('/member/getinfo')
+            .then(function (response) {
+                setAccount(response.data);
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+    
+        api.post('/borrow/list')
+            .then(function (response) {
+                setBooksData(response.data);
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+
+        setIsLoading(false);
+    }, []);
+
+    const books = booksData.map(book =>
     <li key = {book.borrowId} className={styles.bookItem}>
         <div className={styles.bookimage}>
             <img src="example_book.png" alt="Cover image of book" height={180} />
@@ -56,16 +98,40 @@ function MyPage() {
                 </header>
                 <div className={styles.content}>
                     <form className={styles.form}>
-                        <input className={styles.input} type='text' disabled placeholder='이름' value={account.이름} name='이름' prefix='이름' />
-                        <input className={styles.input} type='text' disabled placeholder='학번' value={account.학번} name='학번' prefix='이름' />
-                        <input className={styles.input} type='text' disabled placeholder='전화번호' value={account.전화번호} name='전화번호' prefix='이름' />
+                        <input className={styles.input} type='text' disabled placeholder='이름' value={account.name} name='이름' prefix='이름' />
+                        <input className={styles.input} type='text' disabled placeholder='학번' value={account.studentId} name='학번' prefix='이름' />
+                        <input className={styles.input} type='text' disabled placeholder='전화번호' value={account.phoneNumber} name='전화번호' prefix='이름' />
                     </form>
                     <form className={styles.form}>
-                        <input className={styles.input} type='password' placeholder='현재 비밀번호' name='현재 비밀번호' required />
-                        <input className={styles.input} type='password' placeholder='변경할 비밀번호' name='변경할 비밀번호' required />
-                        <input className={styles.input} type='password' placeholder='변경할 비밀번호 확인' name='변경할 비밀번호 확인' required />
+                        <input 
+                            className={styles.input}
+                            type='password'
+                            name='currentPassword'
+                            value={passwordForm.currentPassword}
+                            onChange={handlePasswordChange}
+                            placeholder='현재 비밀번호'
+                            required
+                        />
+                        <input
+                            className={styles.input}
+                            type='password'
+                            name='newPassword'
+                            value={passwordForm.newPassword}
+                            onChange={handlePasswordChange}
+                            placeholder='변경할 비밀번호'
+                            required
+                        />
+                        <input
+                            className={styles.input}
+                            type='password'
+                            name='confirmPassword'
+                            value={passwordForm.confirmPassword}
+                            onChange={handlePasswordChange}
+                            placeholder='변경할 비밀번호 확인'
+                            required
+                        />
                     </form>
-                    <button className={styles.button} type='button' name='비밀번호 변경'>비밀번호 변경</button>
+                    <button className={styles.button} onClick={() => handleSubmitPassword()} type='button' name='비밀번호 변경'>비밀번호 변경</button>
                 </div>
             </div>
         </div>
@@ -73,4 +139,4 @@ function MyPage() {
     );
 }
 
-export default MyPage
+export default MyPage;
