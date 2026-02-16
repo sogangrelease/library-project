9b30577db4bd65f4274aa69ad5c0ffd72132abed
skyflowlab <179109559+skyflowlab@users.noreply.github.com>
2026-02-17 00:02:18 +0900

    BookCover URL variable

--- src/components/BookItem/index.jsx
+++ src/components/BookItem/index.jsx
@@ -5,6 +5,9 @@ import { useNavigate } from 'react-router-dom';
 const BookItem = ({ book }) => {
   const navigate = useNavigate();
 
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080';
+  const BOOKCOVER_URL = import.meta.env.VITE_BOOKCOVER_URL || '/book-covers/';
+
   const navTo = (path) => {
       navigate(path);
   }
@@ -12,7 +15,7 @@ const BookItem = ({ book }) => {
   return (
     <button onClick={() => navTo(`/detail/${book.id}`)} className={styles.bookItem} title={book.titleMain}>
         <div className={styles.bookCover}>
-            <img src={book.coverUrl || `https://placehold.co/200x300/e0e0e0/909090?text=${book.title}`} alt={`${book.title} cover`} />
+            <img src={API_BASE_URL + BOOKCOVER_URL + book.coverUrl || `https://placehold.co/200x300/e0e0e0/909090?text=${book.title}`} alt={`${book.title} cover`} />
         </div>
         <div className={styles.bookDetails}>
             <h2 className={styles.bookTitle}>{book.titleMain}</h2>
