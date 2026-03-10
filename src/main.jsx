fb8d9eb7ef60a2790a4bcc0be7ae5c38f1318054
riverfog7 <cjw9770@gmail.com>
2026-03-10 17:50:39 +0900

    fix: use hashrouter for static site serving

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { CookiesProvider } from 'react-cookie';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <CookiesProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </CookiesProvider>
   </StrictMode>,
 )
