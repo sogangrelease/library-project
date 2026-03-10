a52a82b0fba8d49531edbc367ff63545f0d16074
skyflowlab <179109559+skyflowlab@users.noreply.github.com>
2026-03-10 18:30:02 +0900

    temporally set image url

--- src/components/BookImage/index.jsx
+++ src/components/BookImage/index.jsx
@@ -1,11 +1,8 @@
 import styles from './BookImage.module.css';
 
 const BookImage = ({ src, alt }) => {
-    const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080';
-    const BOOKCOVER_URL = import.meta.env.VITE_BOOKCOVER_URL || '/book-covers/';
-    
     return <div className={styles.bookImage}>
-        <img src={API_BASE_URL + BOOKCOVER_URL + src} alt={alt} />
+        <img src={src} alt={alt} />
     </div>;
 }
 
