ebe0595daae2ac71f69693563fe9d35ce00c95bf
riverfog7 <cjw9770@gmail.com>
2026-03-10 17:40:56 +0900

    fix: fix case mismatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from '@/components/Header';
+import Header from '@/components/header';
 import SearchPage from '@/pages/SearchPage';
 import MainPage from '@/pages/MainPage';
 import MyPage from '@/pages/MyPage';
